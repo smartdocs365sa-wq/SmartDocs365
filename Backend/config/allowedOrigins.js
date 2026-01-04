80541259df576674dcf77d5aeb3b4702d5b66b28
sivamec24 <sivamec24@gmail.com>
2026-01-04 21:19:07 +0530

    Initial commit - SmartDocs365 project

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,13 +1,9 @@
 const allowedOrigins = [
-  "http://localhost:3001",
-  "http://localhost:3000",
-  "http://localhost",
-  "https://smartdocs365.com",
-  "https://www.smartdocs365.com", // <--- Add this line to be safe
-  "https://admin.smartdocs365.com",
-  "chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop",
-  "chrome-extension://gmmkjpcadciiokjpikmkkmapphbmdjok",
-  'https://your-app.vercel.app'
+  'http://localhost:3000',
+  'http://localhost:3033',
+  'https://smartdocs365.com',
+  'https://www.smartdocs365.com',
+  'https://smart-docs365.vercel.app/' // Your actual Vercel URL
 ];
 
 module.exports = allowedOrigins;
